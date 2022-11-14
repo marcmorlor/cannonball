4cf0ff7218f90f8f4c8930545a03739148ffc4af
Matt Brailsford <me@mattbrailsford.com>
2022-11-14 19:15:54 +0000

    Try an implement dashboard

--- src/main/dashboard/dashboard.cpp
+++ src/main/dashboard/dashboard.cpp
@@ -1,10 +1,9 @@
 #include <stdint.h>
+#include <iostream.h>
+#include <sstream.h>
 #include <wiringPi.h>
-#include <wiringPiI2C.h>
+#include <wiringSerial.h>
 #include "dashboard.hpp"
-
-// Store 7 seg number formats
-unsigned char numbers[] = { 0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0, 0xFE, 0xF6, 0x00 };
 
 Dashboard dashboard;
 
@@ -15,65 +14,14 @@ Dashboard::Dashboard(void) {
 Dashboard::~Dashboard(void) {
 }
 
-bool Dashboard::init(uint8_t addr) {
+bool Dashboard::init(char *device, unsigned int baud) {
 
-  // _i2caddr = addr;
-  // _frame = 0;
-  // _module = wiringPiI2CSetup(_i2caddr);
+  _fd = serialOpen (device, baud);
 
-  // // Init LED states
-  // _ledStates[0] = 0x00;
-  // _ledStates[1] = 0x00;
-  // _ledStates[2] = 0x00;
-  // _ledStates[3] = 0x00;
-  // _ledStates[4] = 0x00;
-  // _ledStates[5] = 0x00;
-
-  // // shutdown
-  // writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x00);
-
-  // delay(10);
-
-  // // out of shutdown
-  // writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x01);
-
-  // // picture mode
-  // writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_CONFIG, ISSI_REG_CONFIG_PICTUREMODE);
-
-  // // Display frame 
-  // writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_PICTUREFRAME, _frame);
-
-  // // Disable unused leds
-  // for(uint8_t f=0; f<8; f++){
-  //   writeRegister8(f, 0x00, 0xFF); // Tacho
-  //   writeRegister8(f, 0x02, 0x1F); // Fuel
-  //   writeRegister8(f, 0x04, 0x01); // Turbo
-  //   writeRegister8(f, 0x06, 0xFF); // Speed 100s
-  //   writeRegister8(f, 0x08, 0xFF); // Speed 10s
-  //   writeRegister8(f, 0x0A, 0xFF); // Speed 1s
-  //   writeRegister8(f, 0x0C, 0x00);
-  //   writeRegister8(f, 0x0E, 0x00);
-  //   writeRegister8(f, 0x10, 0x00);
-
-  //   writeRegister8(f, 0x01, 0x00);
-  //   writeRegister8(f, 0x03, 0x00);
-  //   writeRegister8(f, 0x05, 0x00);
-  //   writeRegister8(f, 0x07, 0x00);
-  //   writeRegister8(f, 0x09, 0x00);
-  //   writeRegister8(f, 0x0B, 0x00);
-  //   writeRegister8(f, 0x0D, 0x00);
-  //   writeRegister8(f, 0x0F, 0x00);
-  //   writeRegister8(f, 0x11, 0x00);
-  // }
-
-  // // Don't sync audio
-  // writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_AUDIOSYNC, 0x0);
-
-  // // set each led to 0 PWM
-  // clearAll(); 
-
-  // // Flag initialization finished
-  // _inited = true;
+  _speed = 0;
+  _turbo = false;
+  _revs = 0;
+  _fuel = MAX_FUEL_LEVEL;
 
   return true;
 }
@@ -87,17 +35,10 @@ void Dashboard::clearAll(void) {
 }
 
 void Dashboard::updateTacho(uint8_t revs) {
-  // if (revs < 0) revs = 0;
-  // if (revs > MAX_TACHO_REVS) revs = MAX_TACHO_REVS;
-  
-  // for(uint8_t x=0; x<8; x++){
-  //   if (x + 1 <= revs){
-  //     drawPixel(x, DASH_TACHO_Y, DASH_LED_PWM);
-  //   } else {
-  //     drawPixel(x, DASH_TACHO_Y, 0);
-  //   }
-  // }
-
+  if (revs < 0) revs = 0;
+  if (revs > MAX_TACHO_REVS) revs = MAX_TACHO_REVS;
+  _revs = revs;
+  updateDashboard();
   return;
 }
 
@@ -107,17 +48,10 @@ void Dashboard::clearTacho(void) {
 }
 
 void Dashboard::updateFuel(uint8_t level) {
-  // if (level < 0) level = 0;
-  // if (level > MAX_FUEL_LEVEL) level = MAX_FUEL_LEVEL;
-  
-  // for(uint8_t x=0; x<5; x++){
-  //   if (x + 1 <= level){
-  //     drawPixel(x, DASH_FUEL_Y, DASH_LED_PWM);
-  //   } else {
-  //     drawPixel(x, DASH_FUEL_Y, 0);
-  //   }
-  // }
-
+  if (level < 0) level = 0;
+  if (level > MAX_FUEL_LEVEL) level = MAX_FUEL_LEVEL;
+  _fuel = level;
+  updateDashboard();
   return;
 }
 
@@ -127,11 +61,8 @@ void Dashboard::clearFuel(void) {
 }
 
 void Dashboard::updateTurbo(bool enabled) {
-  // if (enabled){
-  //   drawPixel(0, DASH_TURBO_Y, DASH_LED_PWM);
-  // } else {
-  //   drawPixel(0, DASH_TURBO_Y, 0);
-  // }
+  _turbo = enabled;
+  updateDashboard();
   return;
 }
 
@@ -141,90 +72,38 @@ void Dashboard::clearTurbo(void) {
 }
 
 void Dashboard::updateSpeed(uint16_t speed) {
-  // if (speed < 0) speed = 0;
-  // if (speed > 999) speed = 999;
-
-  // // Hundreds
-  // uint8_t next100 = DASH_NUMBER_EMPTY;
-  // if (speed > 99) next100 = speed / 100 % 10;
-  // drawNumber(DASH_SPEED_100_Y, next100);
-
-  // // Tens
-  // uint8_t next10 = DASH_NUMBER_EMPTY;
-  // if (speed > 9) next10 = speed / 10 % 10;
-  // drawNumber(DASH_SPEED_10_Y, next10);
-
-  // // Ones
-  // uint8_t next1 = DASH_NUMBER_EMPTY;
-  // if (speed > 0) next1 = speed % 10;
-  // drawNumber(DASH_SPEED_1_Y, next1);
-
+  if (speed < 0) speed = 0;
+  if (speed > 255) speed = 255;
+  _speed = speed;
+  updateDashboard();
   return;
 }
 
 void Dashboard::clearSpeed(void) {
-  drawNumber(DASH_SPEED_100_Y, DASH_NUMBER_EMPTY);
-  drawNumber(DASH_SPEED_10_Y, DASH_NUMBER_EMPTY);
-  drawNumber(DASH_SPEED_1_Y, DASH_NUMBER_EMPTY);
+  updateSpeed(0);
   return;
 }
 
 /*************/
 
-void Dashboard::drawNumber(uint16_t y, uint8_t num) {
-  // if (y != DASH_SPEED_100_Y && y != DASH_SPEED_10_Y && y != DASH_SPEED_1_Y) return;
-  // if (num < 0) num = 0;
-  // if (num > 10) num = 10;
-  
-  // for(uint8_t x=0; x<8; x++){
-  //   unsigned char mask = 0x80 >> x;
-  //   if (numbers[num] & mask){
-  //     drawPixel(x, y, DASH_SEVEN_SEG_PWM);
-  //   } else {
-  //     drawPixel(x, y, 0);
-  //   }
-  // }
+void Dashboard::updateDashboard() {
+  uint16_t t1 = _fuel;
+
+  if (_turbo) {
+    t1 += 100;
+  }
+
+  if (_revs > 0){
+    t1 += (_revs * 8);
+  }
+
+  std::ostringstream ss;
+  ss << std::hex << t1;
+  ss << std::hex << _speed;
+  std::string msg = ss.str();
+
+  serialPuts (_fd, msg.c_str());
 
   return;
 }
 
-void Dashboard::drawPixel(int16_t x, int16_t y, uint16_t color) {
-  // if ((x < 0) || (x >= 9)) return;
-  // if ((y < 0) || (y >= 9)) return;
-  // if (color > 255) color = 255; // PWM 8bit max
-  
-  // uint8_t mask = 0x80 >> x;
-  // if (color > 0 && (_ledStates[y] & mask) != mask){
-  //   setLEDPWM(_frame, x + y*16, color);
-  //   _ledStates[y] |= mask;
-  // } else if (color == 0 && (_ledStates[y] & mask) == mask){
-  //   setLEDPWM(_frame, x + y*16, color);
-  //   _ledStates[y] &= ~(mask);
-  // } else if (!_inited){ // Force initial color
-  //   setLEDPWM(_frame, x + y*16, color);
-  // }
-
-  return;
-}
-
-void Dashboard::setLEDPWM(uint8_t bank, uint8_t lednum, uint8_t pwm) {
-  //if (lednum >= 144) return;
-  //writeRegister8(bank, 0x24+lednum, pwm);
-}
-
-void Dashboard::selectBank(uint8_t b) {
-  // if (b != _lastBank){
-  //   wiringPiI2CWriteReg8(_module, ISSI_COMMANDREGISTER, b);
-  //   _lastBank = b;
-  // }
-}
-
-void Dashboard::writeRegister8(uint8_t bank, uint8_t reg, uint8_t data) {
-  //selectBank(bank);
-  //wiringPiI2CWriteReg8(_module, reg, data);
-}
-
-uint8_t  Dashboard::readRegister8(uint8_t bank, uint8_t reg) {
-  //selectBank(bank);
-  return 0; // wiringPiI2CReadReg8 (_module, reg);
-}
--- src/main/dashboard/dashboard.hpp
+++ src/main/dashboard/dashboard.hpp
@@ -2,38 +2,9 @@
 #define _DASHBOARD_H_
 
 #include <stdint.h>
-#include <wiringPiI2C.h>
-
-#define ISSI_ADDR_DEFAULT 0x74
-
-#define ISSI_REG_CONFIG  0x00
-#define ISSI_REG_CONFIG_PICTUREMODE 0x00
-#define ISSI_REG_CONFIG_AUTOPLAYMODE 0x08
-#define ISSI_REG_CONFIG_AUDIOPLAYMODE 0x18
-
-#define ISSI_CONF_PICTUREMODE 0x00
-#define ISSI_CONF_AUTOFRAMEMODE 0x04
-#define ISSI_CONF_AUDIOMODE 0x08
-
-#define ISSI_REG_PICTUREFRAME  0x01
-
-#define ISSI_REG_SHUTDOWN 0x0A
-#define ISSI_REG_AUDIOSYNC 0x06
-
-#define ISSI_COMMANDREGISTER 0xFD
-#define ISSI_BANK_FUNCTIONREG 0x0B    // helpfully called 'page nine'
-
-#define DASH_SEVEN_SEG_PWM 255
-#define DASH_LED_PWM 120
-
-#define DASH_TACHO_Y 0
-#define DASH_FUEL_Y 1
-#define DASH_TURBO_Y 2
-#define DASH_SPEED_100_Y 3
-#define DASH_SPEED_10_Y 4
-#define DASH_SPEED_1_Y 5
-
-#define DASH_NUMBER_EMPTY 10
+#include <iostream.h>
+#include <sstream.h>
+#include <wiringSerial.h>
 
 class Dashboard {
  public:
@@ -43,7 +14,7 @@ class Dashboard {
   Dashboard(void);
   ~Dashboard(void);
 
-  bool init(uint8_t addr = ISSI_ADDR_DEFAULT);
+  bool init(char *device, unsigned int baud);
   void clearAll(void);
 
   void updateTacho(uint8_t revs);
@@ -59,16 +30,12 @@ class Dashboard {
   void clearSpeed(void);
 
  protected:
-  void selectBank(uint8_t bank);
-  void drawNumber(uint16_t y, uint8_t num);
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
-  void setLEDPWM(uint8_t bank, uint8_t lednum, uint8_t pwm);
-  void writeRegister8(uint8_t bank, uint8_t reg, uint8_t data);
-  uint8_t readRegister8(uint8_t bank, uint8_t reg);
-  uint8_t _i2caddr, _frame, _lastBank, _last100, _last10, _last1;
-  unsigned char _ledStates[6];
-  bool _inited;
-  int _module;
+  int _fd;
+
+  uint8_t _revs;
+  uint8_t _fuel;
+  bool _turbo;
+  uint16_t _speed;
 };
 
 extern Dashboard dashboard;
