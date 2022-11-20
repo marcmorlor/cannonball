952fe59be4cf564ffc0c4b809836bc79c442ae65
Matt Brailsford <me@mattbrailsford.com>
2022-11-20 14:55:45 +0000

    use printf

--- src/main/engine/oinitengine.cpp
+++ src/main/engine/oinitengine.cpp
@@ -303,11 +303,11 @@ void OInitEngine::update_engine()
             if (oinputs.gear) {
                 //ohud.blit_text_new(9, 26, "H");
                 dashboard.update_turbo(true);
-                print("HIGH\n");
+                printf("HIGH\n");
             } else {
                 //ohud.blit_text_new(9, 26, "L");
                 dashboard.update_turbo(false);
-                print("LOW\n");
+                printf("LOW\n");
             }
         //}
 
