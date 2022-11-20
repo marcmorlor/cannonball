023aeb3e530bf1cb6ee4de665d774c259e5a3bcd
Matt Brailsford <me@mattbrailsford.com>
2022-11-20 11:33:11 +0000

    re-enable haptic feedback

--- src/main/engine/ooutputs.cpp
+++ src/main/engine/ooutputs.cpp
@@ -19,9 +19,9 @@
     [MPB] Completely stripped back for use with simple haptic motor
 ***************************************************************************/
 
-// #include <stdint.h>
-// #include <wiringPi.h>
-// #include <wiringPiI2C.h>
+#include <stdint.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
 
 #include "engine/outrun.hpp"
 #include "engine/ocrash.hpp"
@@ -42,101 +42,101 @@ OOutputs::~OOutputs(void)
 // Source: 0xECE8
 void OOutputs::init()
 {
-//   _module = wiringPiI2CSetup(DRV2605_ADDR);
+  _module = wiringPiI2CSetup(DRV2605_ADDR);
 
-//   uint8_t id = readRegister8(DRV2605_REG_STATUS);
+  uint8_t id = readRegister8(DRV2605_REG_STATUS);
   
-//   writeRegister8(DRV2605_REG_MODE, 0x00); // out of standby
+  writeRegister8(DRV2605_REG_MODE, 0x00); // out of standby
   
-//   writeRegister8(DRV2605_REG_RTPIN, 0x00); // no real-time-playback
+  writeRegister8(DRV2605_REG_RTPIN, 0x00); // no real-time-playback
   
-//   writeRegister8(DRV2605_REG_WAVESEQ1, 1); // strong click
-//   writeRegister8(DRV2605_REG_WAVESEQ2, 0);
+  writeRegister8(DRV2605_REG_WAVESEQ1, 1); // strong click
+  writeRegister8(DRV2605_REG_WAVESEQ2, 0);
   
-//   writeRegister8(DRV2605_REG_OVERDRIVE, 0); // no overdrive
+  writeRegister8(DRV2605_REG_OVERDRIVE, 0); // no overdrive
   
-//   writeRegister8(DRV2605_REG_SUSTAINPOS, 0);
-//   writeRegister8(DRV2605_REG_SUSTAINNEG, 0);
-//   writeRegister8(DRV2605_REG_BREAK, 0);
-//   writeRegister8(DRV2605_REG_AUDIOMAX, 0x64);
+  writeRegister8(DRV2605_REG_SUSTAINPOS, 0);
+  writeRegister8(DRV2605_REG_SUSTAINNEG, 0);
+  writeRegister8(DRV2605_REG_BREAK, 0);
+  writeRegister8(DRV2605_REG_AUDIOMAX, 0x64);
   
-//   // ERM open loop
+  // ERM open loop
   
-//   // turn off N_ERM_LRA
-//   writeRegister8(DRV2605_REG_FEEDBACK, readRegister8(DRV2605_REG_FEEDBACK) & 0x7F);
-//   // turn on ERM_OPEN_LOOP
-//   writeRegister8(DRV2605_REG_CONTROL3, readRegister8(DRV2605_REG_CONTROL3) | 0x20);
+  // turn off N_ERM_LRA
+  writeRegister8(DRV2605_REG_FEEDBACK, readRegister8(DRV2605_REG_FEEDBACK) & 0x7F);
+  // turn on ERM_OPEN_LOOP
+  writeRegister8(DRV2605_REG_CONTROL3, readRegister8(DRV2605_REG_CONTROL3) | 0x20);
 
-//   // Run in realtime mode
-//   setMode(DRV2605_MODE_REALTIME);
+  // Run in realtime mode
+  setMode(DRV2605_MODE_REALTIME);
 
-//   // Turn off by default
-//   setRealtimeValue(0x00);
+  // Turn off by default
+  setRealtimeValue(0x00);
 }
 
 void OOutputs::tick()
 {
-    // // If game isn't running, then just exit
-    // if (outrun.game_state != GS_INGAME)
-    // {
-    //     setRealtimeValue(0x00);
-    //     return;
-    // }
+    // If game isn't running, then just exit
+    if (outrun.game_state != GS_INGAME)
+    {
+        setRealtimeValue(0x00);
+        return;
+    }
 
-    // const uint16_t speed = oinitengine.car_increment >> 16;
-    // uint16_t index = 0;
+    const uint16_t speed = oinitengine.car_increment >> 16;
+    uint16_t index = 0;
 
-    // // Car Crashing: Diable Motor once speed below 10
-    // if (ocrash.crash_counter)
-    // {
-    //     if (speed > 10) {
-    //         setRealtimeValue(0x5F);
-    //     } else {
-    //         setRealtimeValue(0x00);
-    //     }
-    //     return;
-    // }
+    // Car Crashing: Diable Motor once speed below 10
+    if (ocrash.crash_counter)
+    {
+        if (speed > 10) {
+            setRealtimeValue(0x5F);
+        } else {
+            setRealtimeValue(0x00);
+        }
+        return;
+    }
 
-    // // Car skidding
-    // //if (ocrash.skid_counter) {
+    // Car skidding
+    //if (ocrash.skid_counter) {
 
-    //   //setRealtimeValue(0x3F);
-    //   //return;
+      //setRealtimeValue(0x3F);
+      //return;
 
-    // //}
+    //}
 
-    // // Normal car movement
-    // if (speed < 10)
-    // {
-    //     setRealtimeValue(0x00);
-    //     return;
-    // } 
+    // Normal car movement
+    if (speed < 10)
+    {
+        setRealtimeValue(0x00);
+        return;
+    } 
 
-    // // Wheels are off road
-    // if (oferrari.wheel_state != OFerrari::WHEELS_ON)
-    // {
-    //     setRealtimeValue(0x3F);
-    //     return;
+    // Wheels are off road
+    if (oferrari.wheel_state != OFerrari::WHEELS_ON)
+    {
+        setRealtimeValue(0x3F);
+        return;
 
-    // } else {
+    } else {
 
-    //   // Wheels are on road but slipping
-    //   if (oferrari.is_slipping){
-    //     setRealtimeValue(0x2F);
-    //     return;
-    //   }
+      // Wheels are on road but slipping
+      if (oferrari.is_slipping){
+        setRealtimeValue(0x2F);
+        return;
+      }
 
-    //   // Wheels spinning
-    //   if (oferrari.car_state == OFerrari::CAR_SMOKE)
-    //   {
-    //       setRealtimeValue(0x2F);
-    //       return;
-    //   }
+      // Wheels spinning
+      if (oferrari.car_state == OFerrari::CAR_SMOKE)
+      {
+          setRealtimeValue(0x2F);
+          return;
+      }
 
-    // }
+    }
 
-    // setRealtimeValue(0x00);
-    // return;
+    setRealtimeValue(0x00);
+    return;
 
     // Car Normal
     /*else if (!ocrash.skid_counter)
@@ -188,20 +188,20 @@ void OOutputs::tick()
 // ------------------------------------------------------------------------------------------------
 
 void OOutputs::setMode(uint8_t mode) {
-  //writeRegister8(DRV2605_REG_MODE, mode);
+  writeRegister8(DRV2605_REG_MODE, mode);
 }
 
 void OOutputs::setRealtimeValue(uint8_t rtp) {
-    // if (rtp != _current_v){
-    //     writeRegister8(DRV2605_REG_RTPIN, rtp);
-    //     _current_v = rtp;
-    // }
+    if (rtp != _current_v){
+        writeRegister8(DRV2605_REG_RTPIN, rtp);
+        _current_v = rtp;
+    }
 }
 
 void OOutputs::writeRegister8(uint8_t reg, uint8_t val) {
-  //wiringPiI2CWriteReg8(_module, reg, val);
+  wiringPiI2CWriteReg8(_module, reg, val);
 }
 
 uint8_t  OOutputs::readRegister8(uint8_t reg) {
-  return 0; // wiringPiI2CReadReg8 (_module, reg);
+  return wiringPiI2CReadReg8 (_module, reg);
 }
